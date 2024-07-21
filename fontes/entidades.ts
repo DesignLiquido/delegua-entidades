b8358827a86667bd4fcb3b32c89e149d6c9c3460
Leonel Sanches da Silva <leonel@designliquido.com.br>
2024-07-21 11:05:01 -0700

    Atualizações de configurações, pacotes, e alguns nomes de funções.

--- fontes/entidades.ts
+++ fontes/entidades.ts
@@ -1,199 +1,238 @@
-import * as sistemaArquivos from 'fs';
+import * as sistemaArquivos from "fs";
 
-import { Importador } from '@designliquido/delegua-node/importador/importador'
-import { Lexador } from '@designliquido/delegua/lexador'
-import { AvaliadorSintatico } from '@designliquido/delegua/avaliador-sintatico'
-import { Classe, Declaracao } from '@designliquido/delegua/declaracoes';
-import { SimboloInterface } from '@designliquido/delegua/interfaces';
-import { RetornoImportador } from '@designliquido/delegua-node/importador';
-import { ObjetoDeleguaClasse } from '@designliquido/delegua/estruturas/objeto-delegua-classe'
+import { Importador } from "@designliquido/delegua-node/importador/importador";
+import { Lexador } from "@designliquido/delegua/lexador";
+import { AvaliadorSintatico } from "@designliquido/delegua/avaliador-sintatico";
+import { Classe, Declaracao } from "@designliquido/delegua/declaracoes";
+import { SimboloInterface } from "@designliquido/delegua/interfaces";
+import { RetornoImportador } from "@designliquido/delegua-node/importador";
+import { ObjetoDeleguaClasse } from "@designliquido/delegua/estruturas/objeto-delegua-classe";
 
-import { pluralizar } from '@designliquido/flexoes';
+import { pluralizar } from "@designliquido/flexoes";
 
-import { ErroTabelaNaoEncontrada } from './erros';
-import { TabelaInterface } from './interfaces/tabela-interface';
+import { ErroTabelaNaoEncontrada } from "./erros";
+import { TabelaInterface } from "./interfaces/tabela-interface";
 
 /**
  * Classe responsável por gerar código SQL com base em modelos de tabelas.
  */
 export class Entidades {
-  importador: Importador
-  arquivos: string[] = []
-  tabelas: TabelaInterface[] = []
+    importador: Importador;
+    arquivos: string[] = [];
+    tabelas: TabelaInterface[] = [];
 
-  /**
-   * Construtor da classe Entidades.
-   * @param diretorio_atual O diretório atual.
-   * @param caminho_modelos O caminho dos modelos de tabelas.
-   * @param tecnologia A tecnologia utilizada.
-   */
-  constructor(
-    private readonly diretorio_atual: string,
-    private readonly caminho_modelos: string,
-    // TODO - Adicionar suporte a tecnologias.
-    // private readonly tecnologia: string 
-  ) {
-    this.importador = new Importador(
-      new Lexador(),
-      new AvaliadorSintatico(),
-      {},
-      {},
-      false
-    )
-  }
-
-  /**
-   * Obtém os nomes dos modelos de tabelas.
-   * @returns Uma lista com os nomes dos arquivos.
-   */
-  pegaNomesModelos(): string[] {
-    const diretorio = `${this.diretorio_atual}/${this.caminho_modelos}`;
-    if (!sistemaArquivos.existsSync(diretorio)) {
-      return [];
+    /**
+     * Construtor da classe Entidades.
+     * @param diretorioAtual O diretório atual.
+     * @param caminhoModelos O caminho dos modelos de tabelas.
+     * @param tecnologia A tecnologia utilizada.
+     */
+    constructor(
+        private readonly diretorioAtual: string,
+        private readonly caminhoModelos: string
+    ) // TODO - Adicionar suporte a tecnologias.
+    // private readonly tecnologia: string
+    {
+        this.importador = new Importador(
+            new Lexador(),
+            new AvaliadorSintatico(),
+            {},
+            {},
+            false
+        );
     }
 
-    return sistemaArquivos.readdirSync(diretorio);
-  }
+    /**
+     * Obtém os nomes dos modelos de tabelas.
+     * @returns Uma lista com os nomes dos arquivos.
+     */
+    obterNomesModelos(): string[] {
+        const diretorio = `${this.diretorioAtual}/${this.caminhoModelos}`;
+        if (!sistemaArquivos.existsSync(diretorio)) {
+            return [];
+        }
 
-  /**
-   * Traduz um tipo para o equivalente em SQL.
-   * @param tipo O tipo a ser traduzido.
-   * @returns O tipo traduzido em SQL.
-   */
-  traduzirTipo(tipo: string): string {
-    switch (tipo) {
-      case 'numero':
-        return 'int'
-      case 'texto':
-        return 'varchar'
-      default:
-        throw new Error(`EntidadesError: O tipo: ${tipo} não é valido.`)
+        return sistemaArquivos.readdirSync(diretorio);
     }
-  }
 
-  /**
-   * Procura uma tabela pelo nome.
-   * @param nome_tabela O nome da tabela a ser procurada.
-   * @returns A tabela encontrada.
-   * @throws {ErroTabelaNaoEncontrada} Se a tabela não for encontrada.
-   */
-  procuraTabela(nome_tabela: string): TabelaInterface { 
-    const tabela = this.tabelas.find(tabela => tabela.nomeTabela === nome_tabela)
-    if (!tabela) throw new ErroTabelaNaoEncontrada(nome_tabela)
-    return tabela
-  }
+    /**
+     * Traduz um tipo para o equivalente em SQL.
+     * @param tipo O tipo a ser traduzido.
+     * @returns O tipo traduzido em SQL.
+     */
+    traduzirTipo(tipo: string): string {
+        switch (tipo) {
+            case "numero":
+                return "int";
+            case "texto":
+                return "varchar";
+            default:
+                throw new Error(
+                    `EntidadesError: O tipo: ${tipo} não é valido.`
+                );
+        }
+    }
 
-  pegaValoresDasPropriedades(tabela: TabelaInterface, classe: ObjetoDeleguaClasse): string { 
-    return tabela.atributos.map(atributo => {
-      const propriedade = Object.entries(classe.propriedades).find(([key, value]) => key === atributo.nome);
-      return propriedade ? propriedade[1] : null;
-    }).join(', ');
-  }
+    /**
+     * Procura uma tabela pelo nome.
+     * @param nomeTabela O nome da tabela a ser procurada.
+     * @returns A tabela encontrada.
+     * @throws {ErroTabelaNaoEncontrada} Se a tabela não for encontrada.
+     */
+    procurarTabela(nomeTabela: string): TabelaInterface {
+        const tabela = this.tabelas.find(
+            (tabela) => tabela.nomeTabela === nomeTabela
+        );
+        if (!tabela) throw new ErroTabelaNaoEncontrada(nomeTabela);
+        return tabela;
+    }
 
-  /**
-   * Gera o código SQL para criar uma tabela.
-   * @param tabela A tabela para a qual o código SQL será gerado.
-   * @returns O código SQL para criar a tabela.
-   */
-  gerarCodigoSQLCriar(classe: ObjetoDeleguaClasse): string {
-    const tabela = this.procuraTabela(classe.classe.simboloOriginal.lexema)
-    const atributosSQL = tabela.atributos.map(atributo => {
-      return `${atributo.nome} ${this.traduzirTipo(atributo.tipo)}`
-    }).join(', ');
-    return `CREATE TABLE ${tabela.nomeTabela} (${atributosSQL});`;
-  }
+    obterValoresDasPropriedades(
+        tabela: TabelaInterface,
+        classe: ObjetoDeleguaClasse
+    ): string {
+        return tabela.atributos
+            .map((atributo) => {
+                const propriedade = Object.entries(classe.propriedades).find(
+                    ([key, value]) => key === atributo.nome
+                );
+                return propriedade ? propriedade[1] : null;
+            })
+            .join(", ");
+    }
 
-  /**
-   * Gera o código SQL para inserir dados em uma tabela.
-   * @param tabela A tabela para a qual o código SQL será gerado.
-   * @returns O código SQL para inserir dados na tabela.
-   */
-  gerarCodigoSQLInserir(classe: ObjetoDeleguaClasse): string {
-    const tabela = this.procuraTabela(classe.classe.simboloOriginal.lexema)
-    const atributosSQL = tabela.atributos.map(atributo => atributo.nome).join(', ');
-    const valoresSQL = this.pegaValoresDasPropriedades(tabela, classe)
-    return `INSERT INTO ${tabela.nomeTabela} (${atributosSQL}) VALUES (${valoresSQL});`;
-  }
+    /**
+     * Gera o código SQL para criar uma tabela.
+     * @param tabela A tabela para a qual o código SQL será gerado.
+     * @returns O código SQL para criar a tabela.
+     */
+    gerarCodigoSQLCriar(classe: ObjetoDeleguaClasse): string {
+        const tabela = this.procurarTabela(
+            classe.classe.simboloOriginal.lexema
+        );
+        const atributosSQL = tabela.atributos
+            .map((atributo) => {
+                return `${atributo.nome} ${this.traduzirTipo(atributo.tipo)}`;
+            })
+            .join(", ");
+        return `CREATE TABLE ${tabela.nomeTabela} (${atributosSQL});`;
+    }
 
-  /**
-   * Gera o código SQL para atualizar dados em uma tabela.
-   * @param tabela A tabela para a qual o código SQL será gerado.
-   * @returns O código SQL para atualizar dados na tabela.
-   */
-  gerarCodigoSQLAtualizar(classe: ObjetoDeleguaClasse): string {
-    const tabela = this.procuraTabela(classe.classe.simboloOriginal.lexema);
-    const atributosSQL = tabela.atributos.map(atributo => {
-      const valorPropriedade = classe.propriedades[atributo.nome];
-      const valorFormatado = typeof valorPropriedade === 'string' ? `'${valorPropriedade}'` : valorPropriedade;
-      return `${atributo.nome} = ${valorFormatado}`;
-    }).join(', ');
-    const id = classe.propriedades['id']; 
-    return `UPDATE ${tabela.nomeTabela} SET ${atributosSQL} WHERE id = ${id};`;
-  }
+    /**
+     * Gera o código SQL para inserir dados em uma tabela.
+     * @param tabela A tabela para a qual o código SQL será gerado.
+     * @returns O código SQL para inserir dados na tabela.
+     */
+    gerarCodigoSQLInserir(classe: ObjetoDeleguaClasse): string {
+        const tabela = this.procurarTabela(
+            classe.classe.simboloOriginal.lexema
+        );
+        const atributosSQL = tabela.atributos
+            .map((atributo) => atributo.nome)
+            .join(", ");
+        const valoresSQL = this.obterValoresDasPropriedades(tabela, classe);
+        return `INSERT INTO ${tabela.nomeTabela} (${atributosSQL}) VALUES (${valoresSQL});`;
+    }
 
-  /**
-   * Gera o código SQL para deletar dados de uma tabela.
-   * @param tabela A tabela para a qual o código SQL será gerado.
-   * @returns O código SQL para deletar dados da tabela.
-   */
-  gerarCodigoSQLDeletar(classe: ObjetoDeleguaClasse): string {
-    const tabela = this.procuraTabela(classe.classe.simboloOriginal.lexema);
-    const id = classe.propriedades['id'];
-    return `DELETE FROM ${tabela.nomeTabela} WHERE id = ${id};`;
-  }
+    /**
+     * Gera o código SQL para atualizar dados em uma tabela.
+     * @param tabela A tabela para a qual o código SQL será gerado.
+     * @returns O código SQL para atualizar dados na tabela.
+     */
+    gerarCodigoSQLAtualizar(classe: ObjetoDeleguaClasse): string {
+        const tabela = this.procurarTabela(
+            classe.classe.simboloOriginal.lexema
+        );
+        const atributosSQL = tabela.atributos
+            .map((atributo) => {
+                const valorPropriedade = classe.propriedades[atributo.nome];
+                const valorFormatado =
+                    typeof valorPropriedade === "string"
+                        ? `'${valorPropriedade}'`
+                        : valorPropriedade;
+                return `${atributo.nome} = ${valorFormatado}`;
+            })
+            .join(", ");
+        const id = classe.propriedades["id"];
+        return `UPDATE ${tabela.nomeTabela} SET ${atributosSQL} WHERE id = ${id};`;
+    }
 
-  /**
-   * Gera o código SQL para selecionar dados de uma tabela.
-   * @param tabela A tabela para a qual o código SQL será gerado.
-   * @returns O código SQL para selecionar dados da tabela.
-   */
-  gerarCodigoSQLSelecionarTodos(classe: ObjetoDeleguaClasse): string {
-    const tabela = this.procuraTabela(classe.classe.simboloOriginal.lexema)
-    return `SELECT * FROM ${tabela.nomeTabela};`;
-  }
+    /**
+     * Gera o código SQL para deletar dados de uma tabela.
+     * @param tabela A tabela para a qual o código SQL será gerado.
+     * @returns O código SQL para deletar dados da tabela.
+     */
+    gerarCodigoSQLExcluir(classe: ObjetoDeleguaClasse): string {
+        const tabela = this.procurarTabela(
+            classe.classe.simboloOriginal.lexema
+        );
+        const id = classe.propriedades["id"];
+        return `DELETE FROM ${tabela.nomeTabela} WHERE id = ${id};`;
+    }
 
-  /**
-   * Gera o código SQL para selecionar um dado de uma tabela.
-   * @param tabela A tabela para a qual o código SQL será gerado.
-   * @returns O código SQL para selecionar um dado da tabela.
-   */
-  gerarCodigoSQLSelecionarUm(classe: ObjetoDeleguaClasse): string {
-    const tabela = this.procuraTabela(classe.classe.simboloOriginal.lexema);
-    const id = classe.propriedades['id'];
-    return `SELECT * FROM ${tabela.nomeTabela} WHERE id = ${id};`;
-  }
+    /**
+     * Gera o código SQL para selecionar dados de uma tabela.
+     * @param tabela A tabela para a qual o código SQL será gerado.
+     * @returns O código SQL para selecionar dados da tabela.
+     */
+    gerarCodigoSQLSelecionarTodos(classe: ObjetoDeleguaClasse): string {
+        const tabela = this.procurarTabela(
+            classe.classe.simboloOriginal.lexema
+        );
+        return `SELECT * FROM ${tabela.nomeTabela};`;
+    }
 
-  /**
-   * Carrega os modelos de tabelas.
-   */
-  iniciar(): void {
-    this.arquivos = this.pegaNomesModelos()
+    /**
+     * Gera o código SQL para selecionar um dado de uma tabela.
+     * @param tabela A tabela para a qual o código SQL será gerado.
+     * @returns O código SQL para selecionar um dado da tabela.
+     */
+    gerarCodigoSQLSelecionarUm(classe: ObjetoDeleguaClasse): string {
+        const tabela = this.procurarTabela(
+            classe.classe.simboloOriginal.lexema
+        );
+        const id = classe.propriedades["id"];
+        return `SELECT * FROM ${tabela.nomeTabela} WHERE id = ${id};`;
+    }
 
-    const conteudos_arquivos: RetornoImportador<SimboloInterface, Declaracao>[] = this.arquivos.map(arquivo => {
-      return this.importador.importar(`${this.diretorio_atual}/${this.caminho_modelos}/${arquivo}`)
-    })
+    /**
+     * Carrega os modelos de tabelas.
+     */
+    iniciar(): void {
+        this.arquivos = this.obterNomesModelos();
 
-    const classes: Classe[] = conteudos_arquivos
-      .map(conteudo => conteudo.retornoAvaliadorSintatico.declaracoes
-        .filter(declaracao => declaracao instanceof Classe) as Classe[])
-      .flat()
+        const conteudosArquivos: RetornoImportador<
+            SimboloInterface,
+            Declaracao
+        >[] = this.arquivos.map((arquivo) => {
+            return this.importador.importar(
+                `${this.diretorioAtual}/${this.caminhoModelos}/${arquivo}`
+            );
+        });
 
-    classes.forEach(classe => {
-      const tabela: TabelaInterface = {
-        nomeTabela: pluralizar(classe.simbolo.lexema),
-        atributos: []
-      }
+        const classes: Classe[] = conteudosArquivos
+            .map(
+                (conteudo) =>
+                    conteudo.retornoAvaliadorSintatico.declaracoes.filter(
+                        (declaracao) => declaracao instanceof Classe
+                    ) as Classe[]
+            )
+            .flat();
 
-      classe.propriedades.forEach(propriedade => {
-        tabela.atributos.push({
-          nome: propriedade.nome.lexema,
-          tipo: propriedade.tipo
-        })
-      })
+        classes.forEach((classe) => {
+            const tabela: TabelaInterface = {
+                nomeTabela: pluralizar(classe.simbolo.lexema),
+                atributos: [],
+            };
 
-      this.tabelas.push(tabela)
-    })
+            classe.propriedades.forEach((propriedade) => {
+                tabela.atributos.push({
+                    nome: propriedade.nome.lexema,
+                    tipo: propriedade.tipo,
+                });
+            });
 
-  }
+            this.tabelas.push(tabela);
+        });
+    }
 }
-
--- testes/entidades.test.ts
+++ testes/entidades.test.ts
@@ -1,140 +1,183 @@
-import * as sistemaArquivos from 'fs';
+import * as sistemaArquivos from "fs";
 
-import { ObjetoDeleguaClasse, DeleguaClasse } from '@designliquido/delegua/estruturas'
-import { Classe, PropriedadeClasse } from '@designliquido/delegua/declaracoes';
-import { SimboloInterface } from '@designliquido/delegua/interfaces';
+import {
+    ObjetoDeleguaClasse,
+    DeleguaClasse,
+} from "@designliquido/delegua/estruturas";
+import { Classe, PropriedadeClasse } from "@designliquido/delegua/declaracoes";
+import { SimboloInterface } from "@designliquido/delegua/interfaces";
 
-import { Entidades } from '../fontes/entidades';
-import { TabelaInterface } from '../fontes/interfaces/tabela-interface';
+import { Entidades } from "../fontes/entidades";
+import { TabelaInterface } from "../fontes/interfaces/tabela-interface";
 
-describe('Entidades', () => {
-  let entidades: Entidades;
-  let classe: ObjetoDeleguaClasse;
+describe("Entidades", () => {
+    let entidades: Entidades;
+    let classe: ObjetoDeleguaClasse;
 
-  beforeEach(() => {
-    entidades = new Entidades(process.cwd(), 'dados')
-    entidades.iniciar()
+    beforeEach(() => {
+        entidades = new Entidades(process.cwd(), "dados");
+        entidades.iniciar();
 
-    let propriedadeClasse = new PropriedadeClasse({
-      hashArquivo: -1,
-      lexema: 'nome',
-      linha: -1,
-      literal: 'nome',
-      tipo: 'nome',
-    } as SimboloInterface, 'texto')
-    let deleguaclass = new DeleguaClasse({
-      hashArquivo: -1,
-      lexema: 'Usuarios',
-      linha: -1,
-      literal: 'Usuarios',
-      tipo: 'classe',
-    } as SimboloInterface, null, {}, [propriedadeClasse])
-    classe = new ObjetoDeleguaClasse(deleguaclass)
-    classe.definir({
-      hashArquivo: -1,
-      lexema: 'nome',
-      linha: -1,
-      literal: 'nome',
-      tipo: 'nome',
-    } as SimboloInterface, 'italo')
-  });
-
-  describe('pegaNomesModelos', () => {
-    it('Esperado que retorno um array de nomes dos arquivo', () => {
-      const expected = sistemaArquivos.readdirSync(`${process.cwd()}/dados`);
-      const actual = entidades.pegaNomesModelos();
-      expect(actual).toEqual(expected);
+        let propriedadeClasse = new PropriedadeClasse(
+            {
+                hashArquivo: -1,
+                lexema: "nome",
+                linha: -1,
+                literal: "nome",
+                tipo: "nome",
+            } as SimboloInterface,
+            "texto"
+        );
+        let deleguaClasse = new DeleguaClasse(
+            {
+                hashArquivo: -1,
+                lexema: "Usuarios",
+                linha: -1,
+                literal: "Usuarios",
+                tipo: "classe",
+            } as SimboloInterface,
+            null,
+            {},
+            [propriedadeClasse]
+        );
+        classe = new ObjetoDeleguaClasse(deleguaClasse);
+        classe.definir(
+            {
+                hashArquivo: -1,
+                lexema: "nome",
+                linha: -1,
+                literal: "nome",
+                tipo: "nome",
+            } as SimboloInterface,
+            "italo"
+        );
     });
 
-    it('Esperado que retorne um array vazio', () => {
-      const expected = [];
-      const actual = new Entidades(process.cwd(), 'dados/nenhum_modelo').pegaNomesModelos();
-      expect(actual).toEqual(expected);
+    describe("obterNomesModelos", () => {
+        it("Esperado que retorno um array de nomes dos arquivo", () => {
+            const expected = sistemaArquivos.readdirSync(
+                `${process.cwd()}/dados`
+            );
+            const actual = entidades.obterNomesModelos();
+            expect(actual).toEqual(expected);
+        });
+
+        it("Esperado que retorne um array vazio", () => {
+            const expected = [];
+            const actual = new Entidades(
+                process.cwd(),
+                "dados/nenhum_modelo"
+            ).obterNomesModelos();
+            expect(actual).toEqual(expected);
+        });
     });
-  });
 
-  describe('traduzirTipo', () => {
-    it('Esperado que retorne o valor int para numero', () => {
-      const resultado = entidades.traduzirTipo('numero')
-      expect(resultado).toBe('int')
-    })
+    describe("traduzirTipo", () => {
+        it("Esperado que retorne o valor int para numero", () => {
+            const resultado = entidades.traduzirTipo("numero");
+            expect(resultado).toBe("int");
+        });
 
-    it('Esperado que retorne o valor varchar para texto', () => {
-      const resultado = entidades.traduzirTipo('texto')
-      expect(resultado).toBe('varchar')
-    })
+        it("Esperado que retorne o valor varchar para texto", () => {
+            const resultado = entidades.traduzirTipo("texto");
+            expect(resultado).toBe("varchar");
+        });
 
-    it('Esperado que retorne um erro para valores não válidos.', () => {
-      expect(() => entidades.traduzirTipo('invalido')).toThrow()
-    })
-  })
+        it("Esperado que retorne um erro para valores não válidos.", () => {
+            expect(() => entidades.traduzirTipo("invalido")).toThrow();
+        });
+    });
 
-  it('Esperado que retorne um erro para tabela não encontrada.', () => { 
-    expect(() => entidades.procuraTabela('tabela_invalida')).toThrow()
-  })
+    it("Esperado que retorne um erro para tabela não encontrada.", () => {
+        expect(() => entidades.procurarTabela("tabela_invalida")).toThrow();
+    });
 
-  it('Esperado que retorne a tabela encontrada.', () => {
-    const tabela: TabelaInterface = entidades.procuraTabela('Usuarios')
-    expect(tabela).toBeTruthy()
-  })
+    it("Esperado que retorne a tabela encontrada.", () => {
+        const tabela: TabelaInterface = entidades.procurarTabela("Usuarios");
+        expect(tabela).toBeTruthy();
+    });
 
-  it('Esperado que retorne a query de criação da tabela.', () => { 
-    const resultado = entidades.gerarCodigoSQLCriar(classe)
-    const esperado = 'CREATE TABLE Usuarios (id int, nome varchar);'
-    expect(resultado).toBe(esperado)
-  })
+    it("Esperado que retorne a query de criação da tabela.", () => {
+        const resultado = entidades.gerarCodigoSQLCriar(classe);
+        const esperado = "CREATE TABLE Usuarios (id int, nome varchar);";
+        expect(resultado).toBe(esperado);
+    });
 
-  it.skip('Esperado que retorne a query de inserção de dados na tabela.', () => {
-    const resultado = entidades.gerarCodigoSQLInserir(classe)
-    const esperado = 'INSERT INTO Usuarios (id, nome) VALUES (?, ?);'
-    expect(resultado).toBe(esperado)
-  })
+    it.skip("Esperado que retorne a query de inserção de dados na tabela.", () => {
+        const resultado = entidades.gerarCodigoSQLInserir(classe);
+        const esperado = "INSERT INTO Usuarios (id, nome) VALUES (?, ?);";
+        expect(resultado).toBe(esperado);
+    });
 
-  it.skip('Esperado que retorne a query de alteração dos dados na tabela.', () => {
-    const resultado = entidades.gerarCodigoSQLAtualizar(classe)
-    const esperado = 'UPDATE Usuarios SET nome = ? WHERE id = ?;'
-    expect(resultado).toBe(esperado)
-  })
-  
-  it.skip('Esperado que retorne a query de exclusão dos dados na tabela.', () => { 
-    const resultado = entidades.gerarCodigoSQLDeletar(classe)
-    const esperado = 'DELETE FROM Usuarios WHERE id = ?;'
-    expect(resultado).toBe(esperado)
-  })
+    it.skip("Esperado que retorne a query de alteração dos dados na tabela.", () => {
+        const resultado = entidades.gerarCodigoSQLAtualizar(classe);
+        const esperado = "UPDATE Usuarios SET nome = ? WHERE id = ?;";
+        expect(resultado).toBe(esperado);
+    });
 
-  it('Esperado que retorne a query de seleção dos dados na tabela.', () => { 
-    const resultado = entidades.gerarCodigoSQLSelecionarTodos(classe)
-    const esperado = 'SELECT * FROM Usuarios;'
-    expect(resultado).toBe(esperado)
-  })
+    it.skip("Esperado que retorne a query de exclusão dos dados na tabela.", () => {
+        const resultado = entidades.gerarCodigoSQLExcluir(classe);
+        const esperado = "DELETE FROM Usuarios WHERE id = ?;";
+        expect(resultado).toBe(esperado);
+    });
 
-  it.skip('Esperado que retorne a query de seleção de um dado na tabela.', () => { 
-    const resultado = entidades.gerarCodigoSQLSelecionarUm(classe)
-    const esperado = 'SELECT * FROM Usuarios WHERE id = ?;'
-    expect(resultado).toBe(esperado)
-  })
+    it("Esperado que retorne a query de seleção dos dados na tabela.", () => {
+        const resultado = entidades.gerarCodigoSQLSelecionarTodos(classe);
+        const esperado = "SELECT * FROM Usuarios;";
+        expect(resultado).toBe(esperado);
+    });
 
-  it('Esperado que retorne um erro para classe não encontrada.', () => {
-    expect(() => entidades.gerarCodigoSQLCriar(new ObjetoDeleguaClasse(new DeleguaClasse()))).toThrow()
-  })
+    it.skip("Esperado que retorne a query de seleção de um dado na tabela.", () => {
+        const resultado = entidades.gerarCodigoSQLSelecionarUm(classe);
+        const esperado = "SELECT * FROM Usuarios WHERE id = ?;";
+        expect(resultado).toBe(esperado);
+    });
 
-  it('Esperado que retorne um erro para classe não encontrada.', () => {
-    expect(() => entidades.gerarCodigoSQLInserir(new ObjetoDeleguaClasse(new DeleguaClasse()))).toThrow()
-  })
+    it("Esperado que retorne um erro para classe não encontrada.", () => {
+        expect(() =>
+            entidades.gerarCodigoSQLCriar(
+                new ObjetoDeleguaClasse(new DeleguaClasse())
+            )
+        ).toThrow();
+    });
 
-  it('Esperado que retorne um erro para classe não encontrada.', () => {
-    expect(() => entidades.gerarCodigoSQLAtualizar(new ObjetoDeleguaClasse(new DeleguaClasse()))).toThrow()
-  })
+    it("Esperado que retorne um erro para classe não encontrada.", () => {
+        expect(() =>
+            entidades.gerarCodigoSQLInserir(
+                new ObjetoDeleguaClasse(new DeleguaClasse())
+            )
+        ).toThrow();
+    });
 
-  it('Esperado que retorne um erro para classe não encontrada.', () => {
-    expect(() => entidades.gerarCodigoSQLDeletar(new ObjetoDeleguaClasse(new DeleguaClasse()))).toThrow()
-  })
+    it("Esperado que retorne um erro para classe não encontrada.", () => {
+        expect(() =>
+            entidades.gerarCodigoSQLAtualizar(
+                new ObjetoDeleguaClasse(new DeleguaClasse())
+            )
+        ).toThrow();
+    });
 
-  it('Esperado que retorne um erro para classe não encontrada.', () => {
-    expect(() => entidades.gerarCodigoSQLSelecionarTodos(new ObjetoDeleguaClasse(new DeleguaClasse()))).toThrow()
-  })
+    it("Esperado que retorne um erro para classe não encontrada.", () => {
+        expect(() =>
+            entidades.gerarCodigoSQLExcluir(
+                new ObjetoDeleguaClasse(new DeleguaClasse())
+            )
+        ).toThrow();
+    });
 
-  it('Esperado que retorne um erro para classe não encontrada.', () => {
-    expect(() => entidades.gerarCodigoSQLSelecionarUm(new ObjetoDeleguaClasse(new DeleguaClasse()))).toThrow()
-  })
+    it("Esperado que retorne um erro para classe não encontrada.", () => {
+        expect(() =>
+            entidades.gerarCodigoSQLSelecionarTodos(
+                new ObjetoDeleguaClasse(new DeleguaClasse())
+            )
+        ).toThrow();
+    });
+
+    it("Esperado que retorne um erro para classe não encontrada.", () => {
+        expect(() =>
+            entidades.gerarCodigoSQLSelecionarUm(
+                new ObjetoDeleguaClasse(new DeleguaClasse())
+            )
+        ).toThrow();
+    });
 });
